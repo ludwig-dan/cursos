ee8ee40a159ea3ea10d7825b662e34b087f88592
Danrlei Anata Ludwig <danrlei.ludwig@compasso.com.br>
2026-07-08 13:31:15 -0300

    classes abstratas

--- estudos/java/curso-de-java-para-iniciantes-com-projetos/herança/TestHeranca.java
+++ estudos/java/curso-de-java-para-iniciantes-com-projetos/herança/TestHeranca.java
@@ -1,58 +1,63 @@
-class Animal{
-    String nome;
-    public void comer(){
-        System.out.println("Animal come");
+// classes abstratas servem para definir funções que serão comuns entre outras classes
+// porém, não pode existir sozinha
+// uma classe abstrata não pode ser instanciada
+abstract class Animal{
+    double peso; // todo animal tem peso
+    public void comer(){ // todo animal come
+        System.out.println("Animal comendo");
     }
 
-    public void fazerSom(){
-        System.out.println("som genérico");
-    }
+    abstract void respirar(); // todo animal respira
+    // a classe abstrata pode declarar métodos abstratos apenas para obrigar que as subclasses implementem o método
 
-    Animal(String nome){
-        this.nome = nome;
+    // pode ou não ter um construtor que será acessado através da palavra super na subclasse
+    Animal(double peso){
+        this.peso = peso;
     }
-}
+}// mas "Animal" depende de mais coisas para existir
 
 class Cachorro extends Animal{
     String raca;
-    public void latir(){
-        System.out.println("Cachorro late");
+
+    public void respirar(){
+        System.out.println("Cachorro respirando");
     }
 
     public void fazerSom(){
-        super.fazerSom();
         System.out.println("auau");
     }
 
-    Cachorro(String nome, String raca){
-        super(nome); // chama o construtor da super classe
+    Cachorro(double peso, String raca){
+        super(peso); // chama o construtor da super classe
         this.raca = raca;
+        System.out.println("O cachorro da raça "+this.raca+" pesa "+this.peso+"kg");
+        super.comer();
     }
 }
 
 class Gato extends Animal{
+    public void respirar(){
+        System.out.println("Gato respirando");
+    }
 
     public void fazerSom(){
         System.out.println("miau");
     }
 
-    Gato(String nome){
-        super(nome);
+    Gato(double peso){
+        super(peso);
+        System.out.println("O gato pesa "+this.peso+"kg");
     }
 }
 
 public class TestHeranca{
     public static void main(String[] args){
-        Animal animal = new Animal("Animal Um");
-        Cachorro cachorro = new Cachorro("Animal Dois", "Raça Dois");
-        Gato gato = new Gato("Animal Três");
-        cachorro.latir();
-        cachorro.comer();
-
-        animal.fazerSom();
+        Cachorro cachorro = new Cachorro(5.0, "Vira lata");
+        cachorro.respirar();
         cachorro.fazerSom();
-        gato.fazerSom();
 
-        System.out.println("Cachorro => "+cachorro.nome+" raça => "+cachorro.raca);
+        Gato gato = new Gato(4.6);
+        gato.respirar();
+        gato.fazerSom();
     }
 }
