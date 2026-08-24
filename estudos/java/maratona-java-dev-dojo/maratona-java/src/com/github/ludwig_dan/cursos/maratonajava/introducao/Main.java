56d4cfc16bc4bb08db2af7b59bceeef795e47557
Danrlei Anata Ludwig <danrlei.ludwig@compasso.com.br>
2026-08-24 14:08:03 -0300

    2026-24-08

---
+++ estudos/java/maratona-java-dev-dojo/maratona-java/src/com/github/ludwig_dan/cursos/maratonajava/introducao/Main.java
@@ -0,0 +1,7 @@
+package com.github.ludwig_dan.cursos.maratonajava.introducao;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
